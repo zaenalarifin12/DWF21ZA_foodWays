27d8173659ede8c756fbee7ecf941df5cc122e07
unknown <zaenalblaze@gmail.com>
2021-03-15 20:11:55 +0700

    remove border profile img

--- src/parts/Header/index.js
+++ src/parts/Header/index.js
@@ -66,7 +66,7 @@ function Header(props) {
   };
 
   return (
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-5 mb-3">
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-5 mb-1">
       <h5 className="my-0 mr-md-auto font-weight-normal">
         <Link to={`/`}>
           <img src="/images/logo.png" />
@@ -122,7 +122,7 @@ function Header(props) {
                 >
                   <img
                     src="/images/profile-img.png"
-                    className="img-thumbnail rounded-circle border border-choco bg-choco"
+                    className=""
                   />
                 </OverlayTrigger>
               </div>
@@ -189,7 +189,7 @@ function Header(props) {
                 >
                   <img
                     src="/images/profile-img.png"
-                    className="img-thumbnail rounded-circle border border-choco bg-choco"
+                    className=""
                   />
                 </OverlayTrigger>
               </div>
